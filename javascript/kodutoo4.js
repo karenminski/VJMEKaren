8b61ffed99ff92a59dff94aefcf2eb6bfc520bba
Karen Minski <43170840+karenminskitlu@users.noreply.github.com>
2021-05-05 10:35:37 +0300

    more progress

--- javascript/kodutoo4.js
+++ javascript/kodutoo4.js
@@ -1,5 +1,5 @@
-let message = "See tõesti töötab!"; //varasemalt kasutati muutujate jaoks lühendit var
-let picurl = "../media/";
+//let message = "See tõesti töötab!";
+let picurl = "../../media/";
 let picnameprefix = "tlu_";
 let picext = ".jpg";
 let minpicnum = 1;
@@ -7,83 +7,64 @@ let maxpicnum = 43;
 let picnum = 1;
 let picchange = 0;
 
-window.onload = function () {
-	//alert("See töötab!");
-	//console.log("Sõnum on: " + message);
-	putOpeningTime();
-	putRandomPic();
-	document.getElementById("tlu_pic").addEventListener("click", putRandomPic);
-	//järgnev on failis clock8.js
-	init_clock();
-	//let timer = setInterval(clockTick, 500); //clearInterval(timer);
-	document.getElementById("nextphoto").addEventListener("click", nextPhoto);
-	document.getElementById("prevphoto").addEventListener("click", prevPhoto);
-	//järgnev on failis music8.js
+window.onload = function(){
+    putOpeningTime();
+    putRandomPic();
+    document.getElementById("tlu_pic").addEventListener("click", putRandomPic);
+	// Failis clock8.js
+    init_clock();
+    //let timer = setInterval(clockTick, 500); //clearInterval(timer);
+    document.getElementById("nextphoto").addEventListener("click", nextPhoto);
+    document.getElementById("prevphoto").addEventListener("click", prevPhoto);
+	// Failis music8.js
 	prepare_music();
 	init_draw();
 }
 
-function nextPhoto() {
-	picnum++;
-	if (picnum > maxpicnum) {
+
+function nextPhoto(){
+	picnum ++;
+	if(picnum > maxpicnum){
 		picnum = minpicnum;
 	}
 	putPhoto();
 }
 
-function prevPhoto() {
-	picnum--;
-	if (picnum < minpicnum) {
+function prevPhoto(){
+	picnum --;
+	if(picnum < minpicnum){
 		picnum = maxpicnum;
 	}
 	putPhoto();
 }
 
-function putRandomPic() {
+function putRandomPic(){
 	let randomnum = minpicnum + Math.round(Math.random() * (maxpicnum - minpicnum));
 	picnum = randomnum;
 	putPhoto();
 }
 
-function putPhoto() {
-	if (picchange % 2 == 0) {
+function putPhoto(){
+	if(picchange%2 == 0){
 		document.getElementById("tlu_pic2").src = picurl + picnameprefix + picnum + picext;
 		document.getElementById("tlu_pic2").style.opacity = 1;
+		document.getElementById("tlu_pic2").classList.add("bluranim");
+		document.getElementById("tlu_pic").classList.remove("bluranim");
+
 	} else {
 		document.getElementById("tlu_pic").src = picurl + picnameprefix + picnum + picext;
 		document.getElementById("tlu_pic2").style.opacity = 0;
+		document.getElementById("tlu_pic").classList.add("bluranim");
+		document.getElementById("tlu_pic2").classList.remove("bluranim");
 	}
-	picchange++;
+	document.getElementById("photo_filename").innerText = picnameprefix + picnum + picext;
+	picchange ++;
 }
 
-function putOpeningTime() {
+function putOpeningTime(){
 	let currenttime = new Date();
 	let currenthour = currenttime.getHours();
 	let currentminute = currenttime.getMinutes();
-	let currentsecond = currenttime.getSeconds();
-	document.getElementById("timeplace").innerHTML = "Leht avati kell: " + currenthour + ":" + currentminute + ":" + currentsecond + ".";
-}
-
-function putOpeningTime() {
-	let currenttime = new Date();
-	let currenthour = currenttime.getHours();
-	let currentminute = currenttime.getMinutes();
-	let currentsecond = currenttime.getSeconds();
-	document.getElementById("timeplace").innerHTML = "Leht avati kell: " + currenthour + ":" + currentminute + ":" + currentsecond + ".";
-}
-
-function clockTick() {
-	let currenttime = new Date();
-	let currenthour = currenttime.getHours();
-	let currentminute = currenttime.getMinutes();
-	let currentsecond = currenttime.getSeconds();
-	let secangle = currentsecond * 6;
-	let minangle = currentminute * 6 + currentsecond * .1;
-	let hourangle = currenthour * 30 + currentminute * .5;
-	//transform: rotate(45deg)
-	document.getElementById("secondhand").style.transform = "rotate(" + secangle + "deg)";
-	document.getElementById("minutehand").style.transform = "rotate(" + minangle + "deg)";
-	document.getElementById("hourhand").style.transform = "rotate(" + hourangle + "deg)";
-	//setTimeout(clockTick, 1000);
-	requestAnimationFrame(clockTick);
+	let currentsecond = currenttime.getSeconds(); 
+	document.getElementById("timeplace").innerHTML = "Leht avati kell: " + currenthour +":" + currentminute + ":" + currentsecond +".";
 }
